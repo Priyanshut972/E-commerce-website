6aba671ec591b8c3d84758848c9368d9c6dec3e1
Priyanshu Tandon <143897258+Priyanshut972@users.noreply.github.com>
2025-05-19 09:59:13 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,15 +6,17 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBoU5TSLVNdYKyBQhZ9989anDX2Ym1AAMA",
-  authDomain: "cptpriyanshu.firebaseapp.com",
-  projectId: "cptpriyanshu",
-  storageBucket: "cptpriyanshu.firebasestorage.app",
-  messagingSenderId: "540180103309",
-  appId: "1:540180103309:web:6506945eea651020fc22c4",
-  measurementId: "G-RFWV2G2WJN"
+  apiKey: "AIzaSyBl3lpRbz8dAuCc9TBwRmggF-HvPO5XPGg",
+  authDomain: "e-commerce-e9e4e.firebaseapp.com",
+  projectId: "e-commerce-e9e4e",
+  storageBucket: "e-commerce-e9e4e.firebasestorage.app",
+  messagingSenderId: "54858298299",
+  appId: "1:54858298299:web:fbd73cc17d28bfd0f0ca9e",
+  measurementId: "G-ZMWPH8ZZ2P"
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const provider = new GoogleAuthProvider();
 export const auth = getAuth();
